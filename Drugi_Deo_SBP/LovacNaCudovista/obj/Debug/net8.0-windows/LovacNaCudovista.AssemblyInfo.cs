5c0accf598c1c1ba16e8bdbb3775d26e38fdbb4c
Klose141 <simeonstojanovic8@gmail.com>
2024-07-04 15:48:47 +0200

    monnoonono

--- Drugi_Deo_SBP/LovacNaCudovista/obj/Debug/net8.0-windows/LovacNaCudovista.AssemblyInfo.cs
+++ Drugi_Deo_SBP/LovacNaCudovista/obj/Debug/net8.0-windows/LovacNaCudovista.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LovacNaCudovista")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090a141fead79415afc0f7dbf6fd5d966e59116e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LovacNaCudovista")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LovacNaCudovista")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
